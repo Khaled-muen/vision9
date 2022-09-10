28fa84d060e5a52b9599d123e3fe07e8bf042913
Khaled-muen <khaledabualgalel6@gmail.com>
2022-09-10 10:18:07 +0300

    added contact route

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get("about",function(){
-    return"hello,world!";
+Route::get('about',function(){
+    return'hello world';
+});
+
+Route::get('contact',function(){
+    return'contact us';
 });
